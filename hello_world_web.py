2fcd41a4d9bf9ef9c390059726f617628e342bb0
Tiago Albineli Motta <timotta@gmail.com>
2022-09-03 12:49:42 -0300

    Nosso site

--- hello_world_web.py
+++ hello_world_web.py
@@ -4,4 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<body style='padding:0; margin:0'><div style='background: black'><h1 style='color: white'>Hello, World!</h1> </div></body>"
+    return """
+        <body style='padding:0; margin:0; text-align: center'>
+            <div style='background: black'>
+                <h1 style='color: white'>Testes de Antonio e Tiago na Web</h1>
+            </div>
+            <img src="/static/IMG_20220903_124138.jpg" width=800 />
+        </body>
+    """
